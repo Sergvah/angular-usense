bb1bd3c799741c3d56ee98fb25bedadcc9e760f2
Sergvah <104735864+Sergvah@users.noreply.github.com>
2023-02-05 14:48:12 +0200

    Well Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,21 @@ import { FormControl, Validators } from '@angular/forms';
 
 type PassStrength = 'empty' | 'easy' | 'medium' | 'strong';
 
-// RegExp.prototype.test()
-//  Функция для поиска сопоставления только букв в указанной строке. Возвращает true или false
 const hasLetters = (str: string) => /[a-zA-Z]/.test(str);
 
-// RegExp.prototype.test()
-//  Функция для поиска сопоставления только цифр в указанной строке. Возвращает true или false
 const hasNumbers = (str: string) => /\d/.test(str);
 
-// RegExp.prototype.test()
-//  Функция для поиска сопоставления только символов в указанной строке. Возвращает true или false
 const hasSymbols = (str: string) => /[^a-zA-Z\d]/.test(str);
 
-// Функция на проверку введеного текста - "pass"
 const getPassStrength = (pass: string): PassStrength => {
-  //  Если текст пустой возвращаем "empty"
   if (!pass.length) return 'empty';
-  // Создаем массив и проверяем сколько введено символов и каких
+
   const strength = [
     hasLetters(pass),
     hasNumbers(pass),
     hasSymbols(pass),
   ].filter(Boolean).length;
-  // Возвращаем строку на основе введенных данных. Если во всех трех есть символы тогда "strong" и тд
+
   return strength === 3
     ? 'strong'
     : strength === 2
